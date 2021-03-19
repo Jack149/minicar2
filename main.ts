f3b192a3afebfdcc7ffd6d91b2e96b77d591fa81
Jack149 <31501037+Jack149@users.noreply.github.com>
2021-03-19 15:21:33 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,70 @@
+k_Bit.carStop()
+basic.pause(100)
+k_Bit.LED_brightness(0)
+k_Bit.Led(COLOR.blue)
+basic.pause(1000)
+k_Bit.Led(COLOR.red)
+basic.pause(1000)
+k_Bit.Led(COLOR.red)
+basic.pause(1000)
+k_Bit.OFFLed()
+basic.pause(200)
+let strip = neopixel.create(DigitalPin.P5, 18, NeoPixelMode.RGB)
+strip.clear()
+basic.pause(200)
 basic.forever(function () {
-	
+    for (let index = 0; index <= 17; index++) {
+        strip.clear()
+        strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+        basic.pause(100)
+    }
+    for (let index = 0; index <= 17; index++) {
+        strip.clear()
+        strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Orange))
+        strip.show()
+        basic.pause(100)
+    }
+    for (let index = 0; index <= 17; index++) {
+        strip.clear()
+        strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Yellow))
+        strip.show()
+        basic.pause(100)
+    }
+    for (let index = 0; index <= 17; index++) {
+        strip.clear()
+        strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+        basic.pause(100)
+    }
+    for (let index = 0; index <= 17; index++) {
+        strip.clear()
+        strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+        basic.pause(100)
+    }
+    for (let index = 0; index <= 17; index++) {
+        strip.clear()
+        strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+        basic.pause(100)
+    }
+    for (let index = 0; index <= 17; index++) {
+        strip.clear()
+        strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+        basic.pause(100)
+    }
+    for (let index = 0; index <= 17; index++) {
+        strip.clear()
+        strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+        basic.pause(100)
+    }
+    for (let index = 0; index <= 17; index++) {
+        strip.clear()
+        strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+        basic.pause(100)
+    }
 })
